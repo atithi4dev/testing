106ab4412fb1c63ae5dcc8e653a8e57ef8c1f5b8
Atithi Singh <singhatithi02@gmail.com>
2025-12-06 20:09:42 +0530

    CHANGES MINIMAL FOR NO ROUTER

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,11 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useState } from "react";
 import HomePage from './Homepage';
-import AboutPage from './AboutPage';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-      </Routes>
-    </Router>
+    <div>
+      <HomePage />
+    </div>
   );
 }
 
