4c6c6cf6129adc082c67b0f95f01282a56c5bd0b
Atithi Singh <singhatithi02@gmail.com>
2026-02-13 15:35:06 +0530

    feat(deploymentWorker): ecs backend build task defination create pipeline stage 1

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,16 @@
 require('dotenv').config(); // Load env variables
 const express = require('express');
-const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  methods: ['GET','POST','PUT','DELETE'],
-  credentials: true
-}));
-
 app.use(express.json());
+
+app.post('/', (req, res) => {
+
+  console.log('Received from frontend:', "HEALTHCHECK");
+  res.json({ message: `Hello user, this is healthcheck route.` });
+});
 
 app.post('/api/send-info', (req, res) => {
   const { name } = req.body;
