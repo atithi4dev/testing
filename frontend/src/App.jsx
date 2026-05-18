a713e6dd32ad6989107cc931acd131de8efc4a25
Atithi Singh <singhatithi02@gmail.com>
2026-05-18 06:09:40 +0530

    minimal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,8 @@ import HomePage from './Homepage';
 function App() {
   return (
     <div>
-      <HomePage />
+      <h1>JUST SAYING HELLO NOTHING ELSE;</h1>
+      {/* <HomePage /> */}
     </div>
   );
 }
