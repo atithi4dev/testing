8196a8eae1d905f788f3dc3acffca20d9ada9fef
Atithi Singh <singhatithi02@gmail.com>
2026-05-19 02:34:52 +0530

    minimal

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(cors({
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  console.log('Received from fronted:', "HEALTHCHECK");
+  console.log('Received from frontend:', "HEALTHCHECK");
   res.send(`Hello user, this is healthcheck route.`);
 });
 
