30cfb4d26793be3c5f230a2ebee948090b223a80
Atithi Singh <singhatithi02@gmail.com>
2026-05-19 01:07:38 +0530

    minimal

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: "*",
-  methods: ['GET','POST','PUT','DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
 
 app.use(express.json());
 
-app.post('/', (req, res) => {
-
+app.get('/', (req, res) => {
   console.log('Received from frontend:', "HEALTHCHECK");
-  res.json({ message: `Hello user, this is healthcheck route.` });
+  res.send(`Hello user, this is healthcheck route.`);
 });
 
 app.post('/api/send-info', (req, res) => {
