cb0afca1db4291b8bf1ab228d644bc05dfc3b7e3
Atithi Singh <singhatithi02@gmail.com>
2026-05-20 08:41:58 +0530

    minimal

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.post('/api/send-info', (req, res) => {
   const { name } = req.body;
 
   if (!name || name.trim() === "") {
-    return res.status(400).json({ message: "Name is requred!" });
+    return res.status(400).json({ message: "Name is required!" });
   }
 
   console.log('Received from frontend:', name);
