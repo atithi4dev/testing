0425f479a2ab84effa3b4b23356f97a1ab369af4
Atithi Singh <singhatithi02@gmail.com>
2026-05-21 06:58:24 +0530

    minimal

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   console.log('Received from frontend:', "HEALTHCHECKKK");
-  res.send(`Hello user, this is idk route.`);
+  res.send(`Hello user.`);
 });
 
 app.post('/api/send-info', (req, res) => {
